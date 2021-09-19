9eb11dfdf891c71e11e23d3c26f9855b20e748b2
Simeon <smnlife1998@gmail.com>
2021-09-19 23:20:22 +0300

    Edit of Movie components
    MovieCard and MovieDetail

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,15 +5,15 @@ import { MovieControls } from "./MovieControls";
 export const MovieCard = ({ movie, type }) => {
   return (
     <div className="movie-card">
-      <Link to={`/movie/${movie.title}`}>
+      <Link to={`/movie/${movie.id}`}>
       <div className="overlay"></div>
       <img
         src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
         alt={`${movie.title} Poster`}
         />
 
-      <MovieControls type={type} movie={movie} />
       </Link>
+      <MovieControls type={type} movie={movie} />
     </div>
   );
 };
--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -6,8 +6,6 @@ import {
   fetchSimilarMovie,
 } from "../service";
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
-import { Modal } from "react-bootstrap";
-import ReactPlayer from "react-player";
 import ReactStars from "react-rating-stars-component";
 import { Link } from "react-router-dom";
 
@@ -33,34 +31,34 @@ export function MovieDetail({ match }) {
 
   genres = detail.genres;
 
-  const MoviePalyerModal = (props) => {
-    const youtubeUrl = "https://www.youtube.com/watch?v=";
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title
-            id="contained-modal-title-vcenter"
-            style={{ color: "#000000", fontWeight: "bolder" }}
-          >
-            {detail.title}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body style={{ backgroundColor: "#000000" }}>
-          <ReactPlayer
-            className="container-fluid"
-            url={youtubeUrl + video.key}
-            playing
-            width="100%"
-          ></ReactPlayer>
-        </Modal.Body>
-      </Modal>
-    );
-  };
+  // const MoviePalyerModal = (props) => {
+  //   const youtubeUrl = "https://www.youtube.com/watch?v=";
+  //   return (
+  //     <Modal
+  //       {...props}
+  //       size="lg"
+  //       aria-labelledby="contained-modal-title-vcenter"
+  //       centered
+  //     >
+  //       <Modal.Header closeButton>
+  //         <Modal.Title
+  //           id="contained-modal-title-vcenter"
+  //           style={{ color: "#000000", fontWeight: "bolder" }}
+  //         >
+  //           {detail.title}
+  //         </Modal.Title>
+  //       </Modal.Header>
+  //       <Modal.Body style={{ backgroundColor: "#000000" }}>
+  //         <ReactPlayer
+  //           className="container-fluid"
+  //           url={youtubeUrl + video.key}
+  //           playing
+  //           width="100%"
+  //         ></ReactPlayer>
+  //       </Modal.Body>
+  //     </Modal>
+  //   );
+  // };
 
   let genresList;
   if (genres) {
@@ -75,59 +73,59 @@ export function MovieDetail({ match }) {
     });
   }
 
-  const castList = casts.slice(0, 4).map((c, i) => {
-    return (
-      <div className="col-md-3 text-center" key={i}>
-        <img
-          className="img-fluid rounded-circle mx-auto d-block"
-          src={c.img}
-          alt={c.name}
-        ></img>
-        <p className="font-weight-bold text-center">{c.name}</p>
-        <p
-          className="font-weight-light text-center"
-          style={{ color: "#5a606b" }}
-        >
-          {c.character}
-        </p>
-      </div>
-    );
-  });
+  // const castList = casts.slice(0, 4).map((c, i) => {
+  //   return (
+  //     <div className="col-md-3 text-center" key={i}>
+  //       <img
+  //         className="img-fluid rounded-circle mx-auto d-block"
+  //         src={c.img}
+  //         alt={c.name}
+  //       ></img>
+  //       <p className="font-weight-bold text-center">{c.name}</p>
+  //       <p
+  //         className="font-weight-light text-center"
+  //         style={{ color: "#5a606b" }}
+  //       >
+  //         {c.character}
+  //       </p>
+  //     </div>
+  //   );
+  // });
 
-  const similarMovieList = similarMovie.slice(0, 4).map((item, index) => {
-    return (
-      <div className="col-md-3 col-sm-6" key={index}>
-        <div className="card">
-          <Link to={`/movie/${item.id}`}>
-            <img className="img-fluid" src={item.poster} alt={item.title}></img>
-          </Link>
-        </div>
-        <div className="mt-3">
-          <p style={{ fontWeight: "bolder" }}>{item.title}</p>
-          <p>Rated: {item.rating}</p>
-          <ReactStars
-            count={item.rating}
-            size={20}
-            color1={"#f4c10f"}
-          ></ReactStars>
-        </div>
-      </div>
-    );
-  });
+  // const similarMovieList = similarMovie.slice(0, 4).map((item, index) => {
+  //   return (
+  //     <div className="col-md-3 col-sm-6" key={index}>
+  //       <div className="card">
+  //         <Link to={`/movie/${item.id}`}>
+  //           <img className="img-fluid" src={item.poster} alt={item.title}></img>
+  //         </Link>
+  //       </div>
+  //       <div className="mt-3">
+  //         <p style={{ fontWeight: "bolder" }}>{item.title}</p>
+  //         <p>Rated: {item.rating}</p>
+  //         <ReactStars
+  //           count={item.rating}
+  //           size={20}
+  //           color1={"#f4c10f"}
+  //         ></ReactStars>
+  //       </div>
+  //     </div>
+  //   );
+  // });
 
   return (
     <div className="container">
       <div className="row mt-2">
-        <MoviePalyerModal
+        {/* <MoviePalyerModal
           show={isOpen}
           onHide={() => {
             setIsOpen(false);
           }}
-        ></MoviePalyerModal>
+        ></MoviePalyerModal> */}
         <div className="col text-center" style={{ width: "100%" }}>
           <img
             className="img-fluid"
-            src={`http://image.tmdb.org/t/p/original/${detail.backdrop_path}`}
+            src={`http://image.tmdb.org/t/p/original/${detail.poster_path}`}
             alt={detail.title}
           ></img>
           <div className="carousel-center">
@@ -193,93 +191,15 @@ export function MovieDetail({ match }) {
         </div>
       </div>
 
-      <div className="row mt-3">
+      {/* <div className="row mt-3">
         <div className="col">
           <p style={{ color: "#5a606b", fontWeight: "bolder" }}>CASTS</p>
         </div>
       </div>
-      <div className="row mt-3">{castList}</div>
-
-      <div className="row mt-3">
-        <div className="col">
-          <p style={{ color: "#5a606b", fontWeight: "bolder" }}>
-            SIMILAR MOVIES
-          </p>
-        </div>
-      </div>
-
-      <div className="row mt-3">{similarMovieList}</div>
+      <div className="row mt-3">{castList}</div> */}
 
       <hr className="mt-5" style={{ borderTop: "1px solid #5a606b" }}></hr>
 
-      <div className="row mt-3 mb-5">
-        <div className="col-md-8 col-sm-6" style={{ color: "#5a606b" }}>
-          <h3>ABOUT ME</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
-            error earum perspiciatis praesentium sint ipsum provident blanditiis
-            pariatur necessitatibus voluptas, cum, atque iste eligendi autem,
-            culpa cupiditate placeat facilis repellat.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-            perspiciatis? Numquam, enim illo voluptatum neque facere aut sed ut
-            dolore nihil? Nulla sit, recusandae ea tenetur rerum deserunt sequi
-            earum?
-          </p>
-          <ul className="list-inline">
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-youtube"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-instagram"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="col-md-4 col-sm-6" style={{ color: "#5a606b" }}>
-          <h3>KEEP IN TOUCH</h3>
-          <ul className="list-unstyled">
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-map-marker-alt"></i> Address:
-                </strong>{" "}
-                city, state, country
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-map-marker-alt"></i> Phone:
-                </strong>{" "}
-                +01 00 00 00
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-envelope"></i> Email:
-                </strong>{" "}
-                info@infomail.com
-              </p>
-            </li>
-          </ul>
-        </div>
-      </div>
     </div>
   );
 }
